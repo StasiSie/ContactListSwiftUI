77b15f447f0f75e81856629f353fa0fb66a3c310
StasiSie <mail.koshaksa@gmail.com>
2022-08-16 20:41:46 +0300

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Игорь Богданов on 16.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
